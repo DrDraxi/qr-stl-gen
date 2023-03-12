65b07353597c4410b7f6ba397a2a64055d008fc8
Draxi <drdraxicz@gmail.com>
2023-03-12 02:27:30 +0100

    Formatted code. Removed debug logs

--- main.py
+++ main.py
@@ -3,44 +3,50 @@ from stl import mesh
 import qrcode
 
 
-def create_box(x, y, size, height, z = 0):
+def create_box(x, y, size, height, z=0):
     # Create a cube with the specified size and height at x and y coordinates
     # Create 8 vertices
-    vertices = np.array([\
-        [x, y, z],
-        [x + size, y, z],
-        [x + size, y + size, z],
-        [x, y + size, z],
-        [x, y, height + z],
-        [x + size, y, height + z],
-        [x + size, y + size, height + z],
-        [x, y + size, height + z]])
-    
+    vertices = np.array(
+        [
+            [x, y, z],
+            [x + size, y, z],
+            [x + size, y + size, z],
+            [x, y + size, z],
+            [x, y, height + z],
+            [x + size, y, height + z],
+            [x + size, y + size, height + z],
+            [x, y + size, height + z],
+        ]
+    )
+
     # Create 12 faces
-    faces = np.array([\
-        [0, 3, 1],
-        [1, 3, 2],
-        [0, 4, 7],
-        [0, 7, 3],
-        [4, 5, 6],
-        [4, 6, 7],
-        [5, 1, 2],
-        [5, 2, 6],
-        [2, 3, 6],
-        [3, 7, 6],
-        [0, 1, 5],
-        [0, 5, 4]])
-    
+    faces = np.array(
+        [
+            [0, 3, 1],
+            [1, 3, 2],
+            [0, 4, 7],
+            [0, 7, 3],
+            [4, 5, 6],
+            [4, 6, 7],
+            [5, 1, 2],
+            [5, 2, 6],
+            [2, 3, 6],
+            [3, 7, 6],
+            [0, 1, 5],
+            [0, 5, 4],
+        ]
+    )
+
     # Create the mesh
     qr_mesh = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
     for i, f in enumerate(faces):
         for j in range(3):
-            qr_mesh.vectors[i][j] = vertices[f[j],:]
-            
+            qr_mesh.vectors[i][j] = vertices[f[j], :]
+
     return qr_mesh
 
 
-def create_mesh_fromQR(img, size, height, add_plate = False):
+def create_mesh_fromQR(img, size, height, add_plate=False):
     # For each black pixel in the QR code, create a cube
     # with the specified size and height
 
@@ -60,32 +66,31 @@ def create_mesh_fromQR(img, size, height, add_plate = False):
                 qr_mesh = create_box(x * size, y * size, size, height)
                 # Append it to the list of meshes
                 qr_meshes.append(qr_mesh)
-                print(f"{x},{y} is black")
-            else:
-                print(f"{x},{y} is white")
 
     # Combine the meshes into a final mesh
     qr_mesh = mesh.Mesh(np.concatenate([m.data for m in qr_meshes]))
 
     # Add a plate to the bottom of the QR code that is one pixel larger
     if add_plate:
-        plate_mesh = create_box(-size, -size, img_size * size + 2 * size, height, z = -height)
-        qr_mesh = mesh.Mesh(np.concatenate([qr_mesh.data, plate_mesh.data]))    
+        plate_mesh = create_box(
+            -size, -size, img_size * size + 2 * size, height, z=-height
+        )
+        qr_mesh = mesh.Mesh(np.concatenate([qr_mesh.data, plate_mesh.data]))
 
     return qr_mesh
 
 
 def main():
     data_in = input("Enter data to encode: ")
-    
+
     add_plate = False
     plate = input("Add plate? (y/n): ")
     if plate == "y":
         add_plate = True
     else:
         add_plate = False
-        
-    
+
+    print("Creating QR code...")
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -94,13 +99,16 @@ def main():
     )
     qr.add_data(data_in)
     qr.make(fit=True)
-        
+    print("QR code created")
+
     img = qr.make_image(fill_color="black", back_color="white")
 
     # Create the mesh
-    qr_mesh = create_mesh_fromQR(img, 1, 1, add_plate = add_plate)
+    print("Creating mesh...")
+    qr_mesh = create_mesh_fromQR(img, 1, 1, add_plate=add_plate)
     # Save the mesh
     qr_mesh.save("qr_mesh.stl")
+    print("Mesh saved")
 
 
 if __name__ == "__main__":
